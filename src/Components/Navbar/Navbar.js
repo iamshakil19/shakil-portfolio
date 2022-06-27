8f79e46eb15fa41ca1d738f75adbf2a4d99c079a
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-06-27 20:08:24 +0600

    react router hash link added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const Navbar = () => {
     return (
@@ -11,25 +12,24 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a href="/#home">Home</a></li>
-                            <li><a href='/#about'>About</a></li>
-                            <li><a href='/#skill'>Skill</a></li>
-                            <li><a href='/#projects'>Projects</a></li>
-                            <li><Link to={"/blog"}>Blog</Link></li>
-                            <li><a href='/#contact'>Contact</a></li>
+                            <li><HashLink smooth to="/#home">Home</HashLink></li>
+                            <li><HashLink smooth to="/#about">About</HashLink></li>
+                            <li><HashLink smooth to="/#skill">Skill</HashLink></li>
+                            <li><HashLink smooth to="/#projects">Projects</HashLink></li>
+                            <li><HashLink smooth to="/blog">Blog</HashLink></li>
+                            <li><HashLink smooth to="/#contact">Contact</HashLink></li>
                         </ul>
                     </div>
                     <Link to="/" class="btn btn-ghost normal-case text-xl font-bold">Shakil</Link>
                 </div>
                 <div class="navbar-end hidden lg:flex">
                     <ul class="menu menu-horizontal p-0 font-bold text-slate-600">
-                        <li><a href="/#home">Home</a></li>
-                        <li><a href='/#about'>About</a></li>
-                        <li><a href='/#skill'>Skill</a></li>
-                        <li><a href='/#projects'>Projects</a></li>
-                        <li><Link to={"/blog"}>Blog</Link></li>
-                        <li><a href='/#contact'>Contact</a></li>
-
+                        <li><HashLink smooth to="/#home">Home</HashLink></li>
+                        <li><HashLink smooth to="/#about">About</HashLink></li>
+                        <li><HashLink smooth to="/#skill">Skill</HashLink></li>
+                        <li><HashLink smooth to="/#projects">Projects</HashLink></li>
+                        <li><HashLink smooth to="/blog">Blog</HashLink></li>
+                        <li><HashLink smooth to="/#contact">Contact</HashLink></li>
                     </ul>
                 </div>
             </div>
